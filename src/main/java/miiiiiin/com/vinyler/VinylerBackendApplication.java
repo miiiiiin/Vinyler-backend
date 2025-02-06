69b3b34ea47161b6d2fa1568a4caa62c2f41ad26
songkyung <min.songkyung@gmail.com>
2025-02-06 18:05:06 +0900

    init: proj init

---
+++ src/main/java/miiiiiin/com/vinyler/VinylerBackendApplication.java
@@ -0,0 +1,13 @@
+package miiiiiin.com.vinyler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinylerBackendApplication.class, args);
+    }
+
+}
