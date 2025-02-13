b3bf863bfd878a2c7914004ae61b8f0dd396e212
miin <43734612+miiiiiin@users.noreply.github.com>
2025-02-13 18:22:37 +0900

    Revert "VNR-3-OAuth설정"

--- src/main/java/miiiiiin/com/vinyler/user/service/RegisterService.java
+++
@@ -1,8 +0,0 @@
-package miiiiiin.com.vinyler.user.service;
-
-import miiiiiin.com.vinyler.user.dto.ServiceRegisterDto;
-import miiiiiin.com.vinyler.user.entity.User;
-
-public interface RegisterService {
-    User registerUser(ServiceRegisterDto dto);
-}
