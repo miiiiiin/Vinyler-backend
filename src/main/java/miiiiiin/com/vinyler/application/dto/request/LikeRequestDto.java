a0e942807dc3e8660426dd600571eea1c0bbe855
songkyung <min.songkyung@gmail.com>
2025-05-26 23:28:29 +0900

    fix: likeRequestDto 수정

--- src/main/java/miiiiiin/com/vinyler/application/dto/request/LikeRequestDto.java
+++ src/main/java/miiiiiin/com/vinyler/application/dto/request/LikeRequestDto.java
@@ -1,28 +1,23 @@
 package miiiiiin.com.vinyler.application.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
-import miiiiiin.com.vinyler.application.entity.vinyl.*;
-
 import java.util.List;
+import miiiiiin.com.vinyler.application.entity.vinyl.ArtistDetail;
+import miiiiiin.com.vinyler.application.entity.vinyl.Format;
+import miiiiiin.com.vinyler.application.entity.vinyl.Image;
+import miiiiiin.com.vinyler.application.entity.vinyl.TrackList;
+import miiiiiin.com.vinyler.application.entity.vinyl.Video;
 
-@Getter
-@Setter
-public class LikeRequestDto {
-    private AlbumInfo albumInfo;
+public record LikeRequestDto(Long discogsId,
+                             String title,
+                             String artistsSort,
+                             String notes,
+                             String status,
+                             String uri,
+                             String releasedFormatted,
+                             List<TrackList> tracklist,
+                             List<Image> images,
+                             List<Format> formats,
+                             List<Video> videos,
+                             List<ArtistDetail> artists) {
 
-    public record AlbumInfo(
-        Long discogsId,
-        String title,
-        String artistsSort,
-        String notes,
-        String status,
-        String uri,
-        String releasedFormatted,
-        List<TrackList> tracklist,
-        List<Image> images,
-        List<Format> formats,
-        List<Video> videos,
-        List<ArtistDetail> artists
-    ) { }
 }
