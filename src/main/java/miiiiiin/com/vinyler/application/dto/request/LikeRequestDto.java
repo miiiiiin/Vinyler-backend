06e8b7913cce5baaccb59535bf78fa5f40201076
songkyung <min.songkyung@gmail.com>
2025-05-27 01:16:46 +0900

    fix: VinylLikeDto, LikeRequestDto snake case 처리

--- src/main/java/miiiiiin/com/vinyler/application/dto/request/LikeRequestDto.java
+++ src/main/java/miiiiiin/com/vinyler/application/dto/request/LikeRequestDto.java
@@ -1,5 +1,6 @@
 package miiiiiin.com.vinyler.application.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import miiiiiin.com.vinyler.application.entity.vinyl.ArtistDetail;
 import miiiiiin.com.vinyler.application.entity.vinyl.Format;
@@ -7,17 +8,20 @@ import miiiiiin.com.vinyler.application.entity.vinyl.Image;
 import miiiiiin.com.vinyler.application.entity.vinyl.TrackList;
 import miiiiiin.com.vinyler.application.entity.vinyl.Video;
 
-public record LikeRequestDto(Long discogsId,
-                             String title,
-                             String artistsSort,
-                             String notes,
-                             String status,
-                             String uri,
-                             String releasedFormatted,
-                             List<TrackList> tracklist,
-                             List<Image> images,
-                             List<Format> formats,
-                             List<Video> videos,
-                             List<ArtistDetail> artists) {
-
+public record LikeRequestDto(
+    @JsonProperty("discogs_id")
+    Long discogsId,
+    String title,
+    @JsonProperty("artists_sort")
+    String artistsSort,
+    String notes,
+    String status,
+    String uri,
+    @JsonProperty("released_formatted")
+    String releasedFormatted,
+    List<TrackList> tracklist,
+    List<Image> images,
+    List<Format> formats,
+    List<Video> videos,
+    List<ArtistDetail> artists) {
 }
