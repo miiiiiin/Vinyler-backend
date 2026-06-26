196af93efe73a4aea2eeda4788447862c003a1c4
songkyung <min.songkyung@gmail.com>
2026-06-26 18:06:43 +0900

    docs: method doc 추가

--- src/main/java/miiiiiin/com/vinyler/user/service/UserServiceImpl.java
+++ src/main/java/miiiiiin/com/vinyler/user/service/UserServiceImpl.java
@@ -161,6 +161,7 @@ public class UserServiceImpl implements UserService {
 
     /**
      * userId의 팔로워 리스트
+     * 나를 팔로우한 사람들 (팔로워 목록)
      */
     @Override
     public List<UserDto> getFollowersByUser(Long userId, User currentUser) {
@@ -172,6 +173,7 @@ public class UserServiceImpl implements UserService {
 
     /**
      * userId가 팔로워 (userId가 팔로잉하고 있는 리스트)
+     * 내가 팔로우한 사람들 (팔로잉 목록)
      */
     @Override
     public List<UserDto> getFollowingsByUser(Long userId, User currentUser) {
